bcd1ed9dd39c5f05e8b99433800053ecf824c3e1
v-s-android <vin.anddev@gmail.com>
2026-06-13 15:25:38 -0400

    Web deployment using Flask

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route('/emotionDetector')
+def emotion_analysis():
+    ''' This code receives the text from the HTML interface and
+       runs sentiment analysis over it using emotion_detector()
+       function. The output returned shows the emotion scores 
+    '''
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyse = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyse)
+
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is response {}.".format(response['anger'], response['disgust'],response['fear'],response['joy'], response['sadness'], response['dominant_emotion'])
+
+@app.route("/")
+def render_index_page():
+   ''' This function initiates the rendering of the main application
+       page over the Flask channel
+   '''
+   return render_template('index.html')
+
+
+if __name__ == "__main__":
+   ''' This functions executes the flask app and deploys it on localhost:5000
+   '''
+   app.run(host="0.0.0.0", port=5000)
+
+
+
+
+
+
